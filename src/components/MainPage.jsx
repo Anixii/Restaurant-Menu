b6a3f2eac43672241867366ed8f58d62f47d0120
Anixii <kenjebekoverbol53@gmail.com>
2023-10-02 20:20:49 +0600

    Главный экран

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import s from './Main.module.css'
+import imgge from '../assets/img/icon.svg'
 import { Image, QRCode } from 'antd'
-import { motion} from 'framer-motion'
+import { motion} from 'framer-motion' 
+import inst from '../assets/img/instagram.png' 
+import whats from '../assets/img/whatsapp.png'
 const MainPage = () => { 
   const nav = useNavigate()
   const onHandleClick = () => { 
@@ -11,15 +14,16 @@ const MainPage = () => {
   return (
     <>  
     <div className={s.main}>
-      <div className={s.main__title}>
-        Добро пожаловать! 
+ 
+      <img className={s.main__img} src={imgge} alt="" />
+      <div className={s.main__title}> 
+        Добро пожаловать в наше заведение
       </div> 
       <div className={s.main__link}> 
-        <motion.button whileTap={{scale:1.1}} onClick={onHandleClick}>Открыть меню</motion.button>
-      </div> 
-      <div className={s.main__qr}>   
-      <QRCode type='svg' value='https://www.instagram.com/itacademy_kb/'/>
-
+        <motion.button className={s.main__button} whileTap={{scale:1.1}} onClick={onHandleClick}>Меню</motion.button>
+      </div>
+      <div className={s.main__contacts}>
+        <img src={whats} alt="" />   +996 508 333 007      <img className={s.inst}src={inst} alt="" />   <Link to={'https://www.instagram.com/talas_daamy/'}>  talas_daamy</Link>
       </div>
     </div>
     </>
